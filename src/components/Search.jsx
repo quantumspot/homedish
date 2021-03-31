6b7559d13a8097eb7942cbbc5d94015562a3aed3
xiaotongli <xiaotongli>
2021-03-30 23:07:45 -0400

    finish search page framework

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clsx from 'clsx';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import InputBase from '@material-ui/core/InputBase';
 import StarIcon from '@material-ui/icons/Star';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+
+// fake sample data
 
 const arraySample = 
     [ {id: 1,
@@ -53,8 +65,49 @@ const arraySample =
       },
     ];
 
+const mealType = [
+    'Breakfast',
+    'Lunch',
+    'Dinner',
+    'Dessert',
+    ];
+
+const rating = [
+    '5 or less',
+    '4 or less',
+    '3 or less',
+    '2 or less',
+    '1 star'
+    ];
+
+const price = [
+    '10 or less',
+    '9 or less',
+    '8 or less',
+    '7 or less',
+    '6 or less',
+    '5 or less',
+    '4 or less',
+    '3 or less',
+    '2 or less',
+    '1 token',
+    ];
+
+// TODO: need to standardize allergens and diets
+
+const preferences = [
+    'Breakfast',
+    'Lunch',
+    'Dinner',
+    'Dessert',
+    ];
+
 const Search = () => {
     // const [arraySample, updateSample] = useState(arraySample);
+
+    function searchCity() {
+        return console.log(document.getElementsByName("search-city-field")[0].value);
+    }
 
     return (
         <div className="search">
@@ -67,20 +120,18 @@ const Search = () => {
                 </div>
                 <div>
                 <InputBase
-                    placeholder="Search…"
+                    name="search-city-field"
+                    placeholder="Search your zip code" 
                     inputProps={{ 'aria-label': 'search' }}
                 />
                 </div>
                 <div>
-                    <ArrowForwardIcon />
+                    <ArrowForwardIcon onClick={() => { searchCity(); }} />
                 </div>
             </div>
 
             <div className="filters">
-                <button>Meal Type</button>
-                <button>Rating</button>
-                <button>Price</button>
-                <button>Preferences</button>
+                <MultipleSelect />
             </div>
 
             <div className="results">
@@ -102,15 +153,156 @@ const Search = () => {
     )
 }
 
-// const Filter = () => {
-//     const [isOpen, setIsOpen] = useState(false);
+// filters components
 
-//     return (
-//         <div classname="filter"></div>
-//         <button onclick="{()" ==""> setIsOpen(!isOpen)} className="filter__button"></button>
-//         Technologies
-//   );
-// }
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      maxWidth: 300,
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
+    noLabel: {
+      marginTop: theme.spacing(3),
+    },
+  }));
+  
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+  
+  function getStyles(name, filters, theme) {
+    return {
+      fontWeight:
+        filters.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
+  
+const MultipleSelect = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [mealTypeFilters, setMealTypes] = useState([]);
+    const [ratingFilters, setRatings] = useState([]);
+    const [priceFilters, setPrices] = useState([]);
+    const [preferencesFilters, setPreferences] = useState([]);
+  
+    const handleMealChange = (event) => {
+      setMealTypes(event.target.value);
+    };
+
+    const handleRatingChange = (event) => {
+      setRatings(event.target.value);
+    };
+
+    const handlePriceChange = (event) => {
+      setPrices(event.target.value);
+    };
+    
+    const handlePrefsChange = (event) => {
+      setPreferences(event.target.value);
+    };
+  
+    // const handleChangeMultiple = (event) => {
+    //   const { options } = event.target;
+    //   const value = [];
+    //   for (let i = 0, l = options.length; i < l; i += 1) {
+    //     if (options[i].selected) {
+    //       value.push(options[i].value);
+    //     }
+    //   }
+    //   setRatings(value);
+    // };
+  
+    return (
+      <div>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-name-label">Meal Type</InputLabel>
+          <Select
+            labelId="demo-mutiple-name-label"
+            id="demo-mutiple-name"
+            multiple
+            value={mealTypeFilters}
+            onChange={handleMealChange}
+            input={<Input />}
+            MenuProps={MenuProps}
+          >
+            {mealType.map((name) => (
+              <MenuItem key={name} value={name} style={getStyles(name, mealTypeFilters, theme)}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-name-label">Rating</InputLabel>
+          <Select
+            labelId="demo-mutiple-name-label"
+            id="demo-mutiple-name"
+            value={ratingFilters}
+            onChange={handleRatingChange}
+            input={<Input />}
+            MenuProps={MenuProps}
+          >
+            {rating.map((name) => (
+              <MenuItem key={name} value={name} style={getStyles(name, ratingFilters, theme)}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-name-label">Price</InputLabel>
+          <Select
+            labelId="demo-mutiple-name-label"
+            id="demo-mutiple-name"
+            value={priceFilters}
+            onChange={handlePriceChange}
+            input={<Input />}
+            MenuProps={MenuProps}
+          >
+            {price.map((name) => (
+              <MenuItem key={name} value={name} style={getStyles(name, priceFilters, theme)}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-mutiple-name-label">Preferences</InputLabel>
+          <Select
+            labelId="demo-mutiple-name-label"
+            id="demo-mutiple-name"
+            multiple
+            value={preferencesFilters}
+            onChange={handlePrefsChange}
+            input={<Input />}
+            MenuProps={MenuProps}
+          >
+            {preferences.map((name) => (
+              <MenuItem key={name} value={name} style={getStyles(name, preferencesFilters, theme)}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+    );
+  }
 
 const Result = ( { key, recipeUrl, recipeName, recipeCook, recipeMealType, allergens, rating, recipePrice }) => {
     const starRating = [];
