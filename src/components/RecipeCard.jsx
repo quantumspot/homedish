faec016ca29adc6e4a28a0e8ce3831cb093f2b35
Sam Filip <samjfilip@gmail.com>
2021-04-01 17:21:02 -0700

    fixed recipe card merge issue

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -18,49 +18,6 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 const RecipeCard = () => {
-  
-    return (
-        <>
-            <form>
-                <p>
-                    <TextField id="outlined-basic" label="Name" variant="outlined" />
-                </p>
-                <p>
-                    <TextField id="outlined-basic" label="Meal Type" variant="outlined" />
-                </p>
-                <p>
-                    <TextField id="outlined-basic" label="Country of Origin" variant="outlined" />
-                </p>
-                <p>
-                    <TextField id="outlined-basic" label="Allergies" variant="outlined" />
-                </p>
-                <p>
-                    <TextField id="outlined-basic" label="Diets" variant="outlined" />
-                </p>
-                <p>
-                    <TextField id="outlined-basic" label="Servings Available" variant="outlined" />
-                </p>
-                <p>
-                    <TextField id="outlined-basic" label="Price per Serving" variant="outlined" />
-                </p>
-                <p>
-                    <TextField
-                        id="outlined-multiline-static"
-                        label="Description"
-                        multiline
-                        rows={4}
-                        // defaultValue="Default Value"
-                        variant="outlined"
-                        />
-                </p>
-                <button>Sign Up</button>
-            </form>
-        </>
-    )
-}
-
-export default RecipeCard;
-
   const classes = useStyles();
 
   const [title, setTitle] = useState("");
@@ -259,4 +216,3 @@ export default RecipeCard;
 };
 
 export default RecipeCard;
-
